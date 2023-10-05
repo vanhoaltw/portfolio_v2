25c725e13afa30d4ef28c5ba97020b0e98f5efb2
hoanguyen <vanhoaltw@gmail.com>
2023-10-05 17:40:19 +0700

    fix: sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,10 +2,11 @@
  * @type {import('next-sitemap').IConfig}
  * @see https://github.com/iamvishnusankar/next-sitemap#readme
  */
+
 module.exports = {
   // !STARTERCONF Change the siteUrl
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  siteUrl: 'https://tsnext-tw.thcl.dev',
+  siteUrl: 'https://hoanguyen.dev',
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [{ userAgent: '*', allow: '/' }],
